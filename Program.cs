0717c7c3018fe03f86c36fb47a0801a1e4805efc
AndradeRevillaErick <erick_andradhe@hotmail.com>
2025-05-08 19:55:43 -0600

    ejercicios del 21 al 25

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿class PracticaEjercicios(){
+    static void Main(string[] args){
+        // MayorQueDiez(11);
+        // ParInpar(12);
+        // ClasificaNumero(0);
+        // ClasificaTernario(-10);
+        Verifica(111);
+    }
+
+    static void MayorQueDiez(int numero){
+        if(numero > 10){
+            Console.WriteLine("Si es mayor");
+            return;
+        } 
+        Console.WriteLine("Nel");
+    }
+
+    static void ParInpar(int numero){
+        if(numero % 2 == 0){
+            Console.WriteLine("ES PAR");
+            return;
+        }
+        Console.WriteLine("ES inPAR");
+    }
+
+    static void ClasificaNumero(int numero){
+        if(numero > 0){
+            Console.WriteLine("ES positivo");
+            return;
+        }else if(numero < 0){
+            Console.WriteLine("ES negativo");
+            return;
+        }
+            Console.WriteLine("es cero");
+    }
+
+    static void ClasificaTernario(int numero){
+            
+        string response = (numero > 0) ? "ES positivo" : numero < 0 ? "Es negativo" : "es cero";
+        
+        Console.WriteLine(response);
+    }
+
+    static void Verifica(int numero){
+        if(numero > 10 && numero < 20){
+            Console.WriteLine("Si es");
+            return;
+        } 
+        Console.WriteLine("Nel");
+    }
+}
